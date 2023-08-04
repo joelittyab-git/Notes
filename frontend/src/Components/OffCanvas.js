79c75a2ec534c45ec5e35f0cd1e57fa00c3631be
joelittyab-git <joeittab@gmail.com>
2023-08-04 23:23:19 +0530

    added navbar adn added navabr icons display functionality

---
+++ frontend/src/Components/OffCanvas.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import './Stylesheets/OffCanvas.css'
+
+function OffCanvas({visibilityStatus, setVisibilityStaus}) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+
+      <Offcanvas show={visibilityStatus} onHide={()=>setVisibilityStaus(false)} data-bs-theme="dark">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+}
+
+export default OffCanvas;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import LoginPage from '../Pages/LoginPage';
+import LoginPage from './Pages/LoginPage';
+import NotesPage from './Pages/NotesPage';
+import NavBar from './Commons/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,10 +15,11 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path='/user/login' Component={<LoginPage/>} />
+        <Route path='/user/login' Component={LoginPage} />
+        <Route path='/notes' Component={NotesPage} />
+        <Route path='/navbar' Component={NavBar}/>
       </Routes>
     </Router>
   </React.StrictMode>
 );
-
 
