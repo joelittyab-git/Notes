80857bd6ae0f2fac1010e7b7a2c0ae44515b6475
joelittyab-git <joeittab@gmail.com>
2023-08-11 00:44:39 +0530

    added new note form component

--- frontend/src/Components/Notes/NewNoteForm.js
+++ frontend/src/Components/Notes/NewNoteForm.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import IconButton from '@mui/material/IconButton'
+import React from 'react';
+import IconButton from '@mui/material/IconButton';
 import AddIcon from '@mui/icons-material/Add';
-import './Stylesheets/NotesPage.css';
 import Box from '@mui/joy/Box';
 import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
@@ -19,82 +18,37 @@ import Button from '@mui/joy/Button';
 import SaveIcon from '@mui/icons-material/Save';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
-import NewNoteForm from '../Components/Notes/NewNoteForm';
 
-
-const blue = {
-  100: '#DAECFF',
-  200: '#b6daff',
-  400: '#3399FF',
-  500: '#007FFF',
-  600: '#0072E5',
-  900: '#003A75',
-};
-
-const grey = {
-  50: '#f6f8fa',
-  100: '#eaeef2',
-  200: '#d0d7de',
-  300: '#afb8c1',
-  400: '#8c959f',
-  500: '#6e7781',
-  600: '#57606a',
-  700: '#424a53',
-  800: '#32383f',
-  900: '#24292f',
-};
-
-const GreyThemedTextField = styled(TextField)(({ theme }) => ({
-  backgroundColor: '#f2f2f2', // Grey background color
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderColor: 'grey', // Grey border color
-    },
-  },
-}));
-
-const NotesPage = () => {
-
-  //Button states
-  const [showNewNote, setShowNewNote] = useState(false);
-
-  //Form states
-  const [formData, setFormData] = useState({
-    title:"",
-    body:"",
-    remind:false
-  })
-
-  //Button actions
-  const createNewNote = () => {
-    console.log("Create new note");
-  }
-  const toggleNewNote = () =>{
-    setShowNewNote(!showNewNote);
-  }
-
+const NewNoteForm = ({onClick, showNewNote}) => {
   return (
     <div>
-{/*--------------------------------------------------------New-note-dialogue-box------------------------------------------------------------------------------------ */}
-      {/* <Backdrop
+     <Backdrop
         sx={{ color: '#fff', zIndex: "9" }}
         open={showNewNote}
-        onClick={toggleNewNote}
+        onClick={onClick}
       >
         <div style={{width:"80%", position:"absolute", top:"50%",height:"85%" , left:"50%", transform:"translate(-50%, -50%)", backdropFilter:"opacity",zIndex:100}} onClick={event=>event.stopPropagation()}>
           <Card sx={{width:"100%",height:"100%", backgroundColor:"#FAFAFA"}}>
             <CardContent>
-              <div className="top" style={{display:'flex', justifyContent:"center", width:"100%"}}>
+              <div className="top">
 
                 <FormControl sx={{width:"100%", display:"flex", justifyContent:"center"}}>
 
                   <FormLabel>Title</FormLabel>
                   <Input placeholder="Example: Dinner Menu...." />
                   <FormHelperText>Enter the title of the note here</FormHelperText><br />
+                
+                </FormControl>
+
+                <FormControl sx={{width:"100%", display:"flex", justifyContent:"center"}}>
 
                   <FormLabel>Body</FormLabel>
                   <Textarea placeholder="Example:  1. Carrot curry..." minRows={10} sx={{width:"90%"}}/>
                   <FormHelperText sx={{paddingBottom:"50px"}}>Enter the note content here</FormHelperText>
+
+                </FormControl>
+
+                <FormControl>
 
                   <FormControlLabel
                     control={
@@ -111,18 +65,9 @@ const NotesPage = () => {
             </CardContent>
           </Card>
         </div>
-      </Backdrop> */}
-      <NewNoteForm onClick={toggleNewNote} showNewNote={showNewNote} />
-{/* ---------------------------------------------------------------------------------------------------------------------------------------------------------------*/}
-
-{/* ----------------------------------------------------Add-Note-button--------------------------------------------------------------------------------------- */}
-      <div className="add-button">
-        <IconButton onClick={toggleNewNote} sx={{backgroundColor:"#1976D2", ":hover":{backgroundColor:"transparent"}}}>
-          <AddIcon fontSize="large" htmlColor="black" sx={{":hover":{color:"#1976D2"}}}/>
-        </IconButton>
-      </div>
+      </Backdrop>
     </div>
-  );
+  )
 }
 
-export default NotesPage
+export default NewNoteForm;
